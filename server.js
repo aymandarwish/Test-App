d323c2e263eaa7ed41a3fe44bf2bab075e2bf4d6
aymandarwish <aymandarwiish@gmail.com>
2021-09-05 02:35:06 +0300

    05-09-2021

--- server.js
+++ server.js
@@ -13,3 +13,34 @@ app.use(bodyParser.json());
 // Cors for cross origin allowance
 const cors = require('cors');
 app.use(cors());
+
+//Initialize the main project folder
+app.use(express.static('website'));
+
+const port = 8000;
+// Spin up the server
+const server = app.listen(port, listening);
+// const server = app.listen(port, () => { console.log(`running on localhost: ${port}`) })
+// Callback to debug
+function listening() {
+    console.log("server running");
+    console.log(`running on localhost: ${port}`)
+};
+
+// TODO-ROUTES!
+app.get('/', function (req, res) {
+    res.send('hello world');
+});
+
+// POST method route
+app.post('/', function (req, res){
+    res.send('POST received')
+});
+
+//Setup a basic POST route in the server side code
+const data = [];
+app.post('/addMovie', addMovie)
+function addMovie (req, res){
+    data.push(req.body)
+    console.log(data)
+}
